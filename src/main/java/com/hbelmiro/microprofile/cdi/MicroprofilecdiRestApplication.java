0eaf70986dbabb066ee20bf25f3f2f1813158fce
Helber Belmiro <helber@Helbers-MacBook-Pro.local>
2020-08-04 06:41:36 -0300

    Added project from MicroProfile Starter

---
+++ src/main/java/com/hbelmiro/microprofile/cdi/MicroprofilecdiRestApplication.java
@@ -0,0 +1,11 @@
+package com.hbelmiro.microprofile.cdi;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class MicroprofilecdiRestApplication extends Application {
+}
